812a587756ec723ae198bc9c28b1795e73f3165e
Fabricio Polato <fabriciopolato@gmail.com>
2020-09-09 11:37:42 -0300

    refactor: add authentication to all company routes

--- src/routes/company.routes.ts
+++ src/routes/company.routes.ts
@@ -5,9 +5,11 @@ import authenticate from '../middlewares/authentication';
 const router = Router();
 const companyControllers = new CompanyControllers();
 
+router.use(authenticate);
+
 router.post('/', companyControllers.create);
 router.get('/', companyControllers.show);
 router.put('/:id', companyControllers.update);
-router.put('/vote/:id', authenticate, companyControllers.updateActivity);
+router.put('/vote/:id', companyControllers.updateActivity);
 
 export default router;
